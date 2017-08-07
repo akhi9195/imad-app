55590ed17d8eaeb02a9b665ce1fe4198b3e29c3b
akhi9195 <akhi9195@gmail.com>
2017-08-07 21:59:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articl-one.html'));
+});
+
+app.get('/artical-two', function (req, res) {
+  res.send("Article one and served here");
+});
+
+app.get('/artical-three', function (req, res) {
+  res.send("Article one and served here");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
