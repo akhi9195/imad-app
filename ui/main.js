1400fb1345312a17a7d28efdc331776168881ef2
akhi9195 <akhi9195@gmail.com>
2017-08-15 22:18:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,24 @@ var button = document.getElementById('counter');
 var counter = 0;
    button.onclick = function(){
 
+var request = new XMLHttprequest();
 
-counter = counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+ request.onreaadystatechange = function(){
+    if(request.readyState ===XMLHttprequest.DONE)
+    {
+        //take some action
+        if(request.status === 200)
+        {
+            var counter= request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    
+}
+
+//make request
+request.open('GET','http://akhi9195.imad.hasura-app.io/counter',true);
+request.send(null);
 };
 
