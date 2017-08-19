f8dae20a14a13cb382b9c6257dd30c89e793076c
akhi9195 <akhi9195@gmail.com>
2017-08-19 16:21:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,22 +8,31 @@ alert("he");
     var request = new XMLHttpRequest();
 
     //capture the response and store it in inner html
-    request.onreaadystatechange = function () {
-        alert("hello");
-      if (request.readyState === XMLHttpRequest.DONE) {
-         //take some action
-        if(request.status === 200)
-        {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
+//     request.onreaadystatechange = function () {
+//         alert("hello");
+//       if (request.readyState === XMLHttpRequest.DONE) {
+//          //take some action
+//         if(request.status === 200)
+//         {
+//             var counter = request.responseText;
+//             var span = document.getElementById('count');
+//             span.innerHTML = counter.toString();
+//         }
+//     }
     
-};
+// };
+
+    var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("count").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://akhi9195.imad.hasura-app.io/counter", true);
 
 //make request
-request.open('GET', 'http://akhi9195.imad.hasura-app.io/counter', true);
+// request.open('GET', 'http://akhi9195.imad.hasura-app.io/counter', true);
 //request.send(null);
 };
 
